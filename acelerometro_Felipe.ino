35176ba9bd70a1904914badb5d5c7b0ae20e0622
felipekako <felipekakosampaio@gmail.com>
2023-03-22 17:30:30 -0300

    criando setup

--- acelerometro_Felipe.ino
+++ acelerometro_Felipe.ino
@@ -1,6 +1,41 @@
+#include <MPU6050_tockn.h>
+#include <write>
+
+#define pinLedX 10
+#define pinLedX 11
+#define pinLedX 16
+
+#define MPU6050_ADDR 0x68
+
+#define DEBUG
+MPU6050 mpu6050(Wire);
+
+float anguloX;
+float anguloY;
+float anguloZ;
+
+unsigned long controleTempo;
+
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(9600);
+Wire.begin();
+mpu6050.begin
+mpu.6050.calcGyro0ffsets(false);
 
+pinMode(pinLedX,OUTPUT);
+pinMode(pinLedY,OUTPUT);
+pinMode(pinLedZ,OUTPUT);
+DigitalWrite(pinLedX,HIGH);
+DigitalWrite(pinLedY,HIGH);
+DigitalWrite(pinLedZ,HIGH);
+delay(500);
+DigitalWrite(pinLedX,LOW);
+DigitalWrite(pinLedX,LOW);
+DigitalWrite(pinLedX,LOW);
+#ifdef DEBUG
+Serial.print("fim setup")
+#endif
 }
 
 void loop() {
